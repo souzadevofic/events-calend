0a18088994f75f6faae13be11a262bcddbba6d49
Alisson Souza <alissonso432@gmail.com>
2024-12-18 15:56:08 -0300

    ajustando a cor do background-color do componente modal

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import './Modal.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './Modal.css';
 
 export function Modal({ children, onClose }) {
     return (
         <div className="modal-overlay" onClick={onClose}>
-            <div className="modal-dialog modal-content" onClick={(e) => e.stopPropagation()}>
-
-                <div className="modal-body">
-                    {children}
-                </div>
-                <div className="container-btn">
-                    <button 
-                        type="button" 
-                        className="btn-fechar btn btn-secondary" 
-                        onClick={onClose}
-                    >
-                        Fechar
-                    </button>
-                    <button type="button" className="btn-save btn btn-primary">Salvar</button>
-                </div>
+        <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+            <div className="modal-body">
+                {children}
+            </div>
+            <div className="container-btn">
+                <button 
+                    type="button" 
+                    className="btn-fechar btn btn-secondary" 
+                    onClick={onClose}
+                >
+                    Fechar
+                </button>
+                <button type="button" className="btn-save btn btn-primary">Salvar</button>
             </div>
         </div>
+    </div>
+    
     );
 }
